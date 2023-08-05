183447c9b2d2593c5a84d97acb19b230c65dcdac
Grégory Marini <greg.marini@hotmail.fr>
2023-08-05 11:59:34 +0200

    add DELETE method in route

--- src/app/api/todos/route.ts
+++ src/app/api/todos/route.ts
@@ -2,8 +2,26 @@ import { NextResponse } from 'next/server';
 
 const DATA_SOURCE_URL = 'https://jsonplaceholder.typicode.com/todos';
 
+const API_KEY: string = process.env.DATA_API_KEY as string;
+
 export async function GET(request: Request) {
   const res = await fetch(DATA_SOURCE_URL);
-  const data: Todo[] = await res.json();
-  return NextResponse.json(data);
+  const todos: Todo[] = await res.json();
+  return NextResponse.json(todos);
+}
+
+export async function DELETE(request: Request) {
+  const { id }: Partial<Todo> = await request.json();
+
+  if (!id) return NextResponse.json({ message: 'Todo id is required' });
+
+  await fetch(`${DATA_SOURCE_URL}/${id}`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+      'API-Key': API_KEY,
+    },
+  });
+
+  return NextResponse.json({ message: `Todo ${id} deleted` });
 }
